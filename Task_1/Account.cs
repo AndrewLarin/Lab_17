491a3e0f990e1fe287fa6f03fc721cb6b3f9efa1
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-24 20:48:09 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Account <T>
+    {
+        private T num;
+        public T Num { get { return num; } set { num = value; } }
+
+        private double balance;
+        public double Balance { get { return balance; } set { balance = value; } }
+
+        private string name;
+        public string Name { get { return name; } set { name = value; } }
+
+        public string GetInfo()
+        {
+            return $"{Num} {Name} {Balance}";
+        }
+
+
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер счета");
+            int NumInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс счета");
+            double balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца");
+            string name = Console.ReadLine();
+
+            Account<int> account1 = new Account<int>() { Num = NumInt, Balance = balance, Name = name};
+
+            Console.WriteLine("Введите буквенный номер счета");
+            string NumString = Console.ReadLine();
+            Console.WriteLine("Введите баланс счета");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца");
+            name = Console.ReadLine();
+            Account<string> account2 = new Account<string>() { Num = NumString, Balance = balance, Name = name };
+            Console.WriteLine(account1.GetInfo());
+            Console.WriteLine(account2.GetInfo());
+
+            Console.ReadLine();
+        }
+    }
+}
